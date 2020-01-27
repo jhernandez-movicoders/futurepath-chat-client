b268445fd390b533a73d07ba6fdf911ab165fb31
Daniel Gomez <daniel.gomez@movicoders.com>
2020-01-27 12:51:46 +0100

    feature/Unify

--- ChatClient/src/dev/futurepath/controllers/IncomingMsgController.java
+++ ChatClient/src/dev/futurepath/controllers/IncomingMsgController.java
@@ -25,8 +25,8 @@ public class IncomingMsgController implements RefreshChat.ChatListener {
     private final String USERROOM = "USERROOM";
     private final String USER = "USER";
     private final String ROOM = "ROOM";
-    
-    private RefreshChat thread;
+
+    private RefreshChat thread; //debería ser static??
 
     public IncomingMsgController(ChatView chatview, RoomListView roomListView) {
         this.chatview = new ChatView();
@@ -43,37 +43,44 @@ public class IncomingMsgController implements RefreshChat.ChatListener {
     }
 
     @Override
-    public void onMessageReceived() {
+    public void onMessageReceived() { //Este método podría simplente devolver strings y después hacer otros controllers. Un factory pattern para elegir controller???
         try {
             System.out.println("entro :D");
             //COMMAND USER MESSAGE
-            
+
             String serverMsg = ConnectController.in.readUTF();
-            
-            System.out.println("server msg "+serverMsg);
-            //String serverMsg = "MESSAGE PEPE Hola dani hace tiempo que no hablamos, te echo de menos";
-            String[] parts = serverMsg.split(SEP);//the split method can be changed depending on the received command
-            System.out.println("parts "+parts[0]);
+
+            String[] parts = serverMsg.split(SEP);
+            System.out.println("MSG: " + parts[0]);
             if (parts[0].equals(MESSAGE)) {
                 String textMsg = "";
                 for (int i = 2; i < parts.length; i++) {
                     textMsg += parts[i] + " ";
                 }
                 String message = "<" + parts[1] + "> " + textMsg;
-                //chatview.getMessagesTextArea().append(message);
-                System.out.println(message);
+                chatview.getMessagesTextArea().append(message);
                 serverMsg = "";
             }
-            if (parts[0] == USERROOM) {
+            else if (parts[0].equals(USERROOM)) {
                 DefaultListModel<String> model = new DefaultListModel<>();
                 for (int i = 1; i < parts.length; i++) {
-                    ((DefaultListModel) model).addElement(parts[i]);
+                    model.addElement(parts[i]);
                 }
                 this.chatview.getUserList().setModel(model);
             }
-            if (parts[0] == ROOM){
+            else if (parts[0].equals(ROOM)) {
+                DefaultListModel<String> model = new DefaultListModel<>();
                 
+                for (int i = 1; i < parts.length; i++) {
+                    model.addElement(parts[i]);
+                }
+                System.out.println(parts[1]);
+                
+                //Es podible que tengamos poner el panel RoomListView aquí pues al conectar devuelve ya una lista de rooms
+                this.roomListView.getRoomList().setModel(model);
             }
+            
+            //Un if más para los errores que salte 
         } catch (IOException ex) {
             Logger.getLogger(IncomingMsgController.class.getName()).log(Level.SEVERE, null, ex);
         }
